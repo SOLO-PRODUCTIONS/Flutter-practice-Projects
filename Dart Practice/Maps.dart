deb2f4252bea07e2198066afb80bf3a1f6d2bfd9
omsolanki-MD <omsolanki.midnight@gmail.com>
2026-01-10 09:32:15 +0530

    Refactor code structure for improved readability and maintainability

--- Dart Practice/Maps.dart
+++ Dart Practice/Maps.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 void main() {
+  // --------------------------------------
   var first_map = {
     "description": "map is same as the objects in js",
     "Structure of data": "key:value pair",
@@ -11,6 +12,29 @@ void main() {
   };
 
   stdout.writeln(first_map["description"]);
-  final disadvantage = first_map["disadvantage"] as Map<String, dynamic>;
-  stdout.writeln(disadvantage["nested_Object_key"]);
+  final nested_map = first_map["nested_map"] as Map<String, dynamic>;
+  stdout.writeln(nested_map["nested_Object_key"]);
+  // ----------------------------------------------------
+  var user = {
+    "userName":"",
+    "email":"",
+    "age":18,
+    "is_logged_in":false,
+    "Profile" : {
+      "name" : "",
+      "avatar" : "",
+      "Experience_in_year": 0
+    }
+  };
+
+  user["userName"] = "soloedits" ;
+  user["email"] = "solo@solo.com";
+  user["password"] = "";
+  user["is_logged_in"] = true ;
+  var profile = user["Profile"] as Map<String, dynamic>;
+  profile["name"] = "SOLOEDITS" ;
+  profile["avatar"] = "https://yt3.googleusercontent.com/mu3cCH1RjrYCGrZD0n3k41tLPInFisJrpwS3sy4_o9ZG0BzGv1-Xwh4rR0l5AJc5Tkkrmhca=s160-c-k-c0x00ffffff-no-rj";
+  profile["password"] = "solo123" ;
+
+  stdout.writeln("user ${user} type ${user.runtimeType}");
 }
